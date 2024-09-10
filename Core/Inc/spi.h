d7c827d437bac34d0f667c9d506132870135b508
phonght32 <thanhphongho1998@gmail.com>
2024-09-10 19:36:48 +0700

    update indent

--- Core/Inc/spi.h
+++ Core/Inc/spi.h
@@ -24,17 +24,26 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+
 /* USER CODE BEGIN Includes */
 /* USER CODE END Includes */
+
 extern SPI_HandleTypeDef hspi2;
+
 /* USER CODE BEGIN Private defines */
 /* USER CODE END Private defines */
+
 void MX_SPI2_Init(void);
+
 /* USER CODE BEGIN Prototypes */
 /* USER CODE END Prototypes */
+
 #ifdef __cplusplus
 }
 #endif
+
 #endif /* __SPI_H__ */
+
--- Core/Src/adc.c
+++ Core/Src/adc.c
@@ -22,121 +22,143 @@
 
 /* USER CODE BEGIN 0 */
 /* USER CODE END 0 */
+
 ADC_HandleTypeDef hadc2;
+
 /* ADC2 init function */
 void MX_ADC2_Init(void)
 {
-    /* USER CODE BEGIN ADC2_Init 0 */
-    /* USER CODE END ADC2_Init 0 */
-    ADC_ChannelConfTypeDef sConfig = {0};
-    /* USER CODE BEGIN ADC2_Init 1 */
-    /* USER CODE END ADC2_Init 1 */
-    /** Common config
-    */
-    hadc2.Instance = ADC2;
-    hadc2.Init.ScanConvMode = ADC_SCAN_ENABLE;
-    hadc2.Init.ContinuousConvMode = ENABLE;
-    hadc2.Init.DiscontinuousConvMode = DISABLE;
-    hadc2.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-    hadc2.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-    hadc2.Init.NbrOfConversion = 4;
-    if (HAL_ADC_Init(&hadc2) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    /** Configure Regular Channel
-    */
-    sConfig.Channel = ADC_CHANNEL_0;
-    sConfig.Rank = ADC_REGULAR_RANK_1;
-    sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
-    if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    /** Configure Regular Channel
-    */
-    sConfig.Rank = ADC_REGULAR_RANK_2;
-    if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    /** Configure Regular Channel
-    */
-    sConfig.Rank = ADC_REGULAR_RANK_3;
-    if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    /** Configure Regular Channel
-    */
-    sConfig.Rank = ADC_REGULAR_RANK_4;
-    if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    /* USER CODE BEGIN ADC2_Init 2 */
-    /* USER CODE END ADC2_Init 2 */
+
+  /* USER CODE BEGIN ADC2_Init 0 */
+  /* USER CODE END ADC2_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC2_Init 1 */
+  /* USER CODE END ADC2_Init 1 */
+
+  /** Common config
+  */
+  hadc2.Instance = ADC2;
+  hadc2.Init.ScanConvMode = ADC_SCAN_ENABLE;
+  hadc2.Init.ContinuousConvMode = ENABLE;
+  hadc2.Init.DiscontinuousConvMode = DISABLE;
+  hadc2.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc2.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc2.Init.NbrOfConversion = 4;
+  if (HAL_ADC_Init(&hadc2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Rank = ADC_REGULAR_RANK_2;
+  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Rank = ADC_REGULAR_RANK_3;
+  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Rank = ADC_REGULAR_RANK_4;
+  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC2_Init 2 */
+  /* USER CODE END ADC2_Init 2 */
+
 }
+
 void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
 {
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    if(adcHandle->Instance==ADC2)
-    {
-    /* USER CODE BEGIN ADC2_MspInit 0 */
-    /* USER CODE END ADC2_MspInit 0 */
-        /* ADC2 clock enable */
-        __HAL_RCC_ADC2_CLK_ENABLE();
-        __HAL_RCC_GPIOA_CLK_ENABLE();
-        __HAL_RCC_GPIOB_CLK_ENABLE();
-        /**ADC2 GPIO Configuration
-        PA0-WKUP         ------> ADC2_IN0
-        PA1         ------> ADC2_IN1
-        PA2         ------> ADC2_IN2
-        PA3         ------> ADC2_IN3
-        PA4         ------> ADC2_IN4
-        PA5         ------> ADC2_IN5
-        PA6         ------> ADC2_IN6
-        PA7         ------> ADC2_IN7
-        PB0         ------> ADC2_IN8
-        PB1         ------> ADC2_IN9
-        */
-        GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                                                    |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
-        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-        GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
-        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-    /* USER CODE BEGIN ADC2_MspInit 1 */
-    /* USER CODE END ADC2_MspInit 1 */
-    }
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(adcHandle->Instance==ADC2)
+  {
+  /* USER CODE BEGIN ADC2_MspInit 0 */
+  /* USER CODE END ADC2_MspInit 0 */
+    /* ADC2 clock enable */
+    __HAL_RCC_ADC2_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**ADC2 GPIO Configuration
+    PA0-WKUP     ------> ADC2_IN0
+    PA1     ------> ADC2_IN1
+    PA2     ------> ADC2_IN2
+    PA3     ------> ADC2_IN3
+    PA4     ------> ADC2_IN4
+    PA5     ------> ADC2_IN5
+    PA6     ------> ADC2_IN6
+    PA7     ------> ADC2_IN7
+    PB0     ------> ADC2_IN8
+    PB1     ------> ADC2_IN9
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN ADC2_MspInit 1 */
+  /* USER CODE END ADC2_MspInit 1 */
+  }
 }
+
 void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle)
 {
-    if(adcHandle->Instance==ADC2)
-    {
-    /* USER CODE BEGIN ADC2_MspDeInit 0 */
-    /* USER CODE END ADC2_MspDeInit 0 */
-        /* Peripheral clock disable */
-        __HAL_RCC_ADC2_CLK_DISABLE();
-        /**ADC2 GPIO Configuration
-        PA0-WKUP         ------> ADC2_IN0
-        PA1         ------> ADC2_IN1
-        PA2         ------> ADC2_IN2
-        PA3         ------> ADC2_IN3
-        PA4         ------> ADC2_IN4
-        PA5         ------> ADC2_IN5
-        PA6         ------> ADC2_IN6
-        PA7         ------> ADC2_IN7
-        PB0         ------> ADC2_IN8
-        PB1         ------> ADC2_IN9
-        */
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                                                    |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
-        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_0|GPIO_PIN_1);
-    /* USER CODE BEGIN ADC2_MspDeInit 1 */
-    /* USER CODE END ADC2_MspDeInit 1 */
-    }
+
+  if(adcHandle->Instance==ADC2)
+  {
+  /* USER CODE BEGIN ADC2_MspDeInit 0 */
+  /* USER CODE END ADC2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_ADC2_CLK_DISABLE();
+
+    /**ADC2 GPIO Configuration
+    PA0-WKUP     ------> ADC2_IN0
+    PA1     ------> ADC2_IN1
+    PA2     ------> ADC2_IN2
+    PA3     ------> ADC2_IN3
+    PA4     ------> ADC2_IN4
+    PA5     ------> ADC2_IN5
+    PA6     ------> ADC2_IN6
+    PA7     ------> ADC2_IN7
+    PB0     ------> ADC2_IN8
+    PB1     ------> ADC2_IN9
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
+
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_0|GPIO_PIN_1);
+
+  /* USER CODE BEGIN ADC2_MspDeInit 1 */
+  /* USER CODE END ADC2_MspDeInit 1 */
+  }
 }
+
 /* USER CODE BEGIN 1 */
 /* USER CODE END 1 */
