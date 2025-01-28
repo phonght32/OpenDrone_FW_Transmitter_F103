1e0e097c7f935021f9a4c890f49c5a599cf8cc9d
phonght32 <thanhphongho1998@gmail.com>
2025-01-28 11:45:14 +0700

    increase number of cycles for getting adc value

--- Core/Src/adc.c
+++ Core/Src/adc.c
@@ -55,7 +55,7 @@ void MX_ADC2_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_13CYCLES_5;
   if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
   {
     Error_Handler();
